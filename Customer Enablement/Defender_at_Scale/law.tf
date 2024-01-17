82e394f5b2a845fa973934d24d2219d42a4a0696
cshea15 <chashea@microsoft.com>
2024-01-17 13:26:26 -0500

    adding files

---
+++ Customer Enablement/Defender_at_Scale/law.tf
@@ -0,0 +1,17 @@
+## Current Management Group
+data "azurerm_management_group" "example" {
+  name = var.mgmt_group_name
+}
+resource "azurerm_resource_group" "security_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+## Allows you to create LAW and onboard
+
+resource "azurerm_log_analytics_workspace" "la_workspace" {
+  name                = "mdc-security-workspace"
+  location            = azurerm_resource_group.security_rg.location
+  resource_group_name = azurerm_resource_group.security_rg.name
+  sku                 = "PerGB2018"
+}
