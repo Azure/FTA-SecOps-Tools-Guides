226b76a99b8fa1721059537fba7ca2325243a69d
cshea15 <chashea@microsoft.com>
2024-01-24 15:57:49 -0500

    updating file structure

--- Customer Enablement/Defender_at_Scale/Terraform/dfc.tf
+++ Customer Enablement/Defender_at_Scale/Terraform/dfc.tf
@@ -1,4 +1,14 @@
 ## Policy Assignment
+resource "azurerm_management_group_policy_assignment" "enableASC_assignment" {
+ name                 = "enableASC_assignment"
+ display_name         = "Enable Microsoft Defender for Cloud on your subscription"
+ policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/ac076320-ddcf-4066-b451-6154267e8ad2"
+ management_group_id  = data.azurerm_management_group.example.id
+  location = var.location
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
 resource "azurerm_management_group_policy_assignment" "mcsb_assignment" {
   name                 = "mcsb"
